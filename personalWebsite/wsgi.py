ab935d50767a9f45ec1ddd711806dd064c865df6
Joshua Tumlinson <jdtumlinson@gmail.com>
2025-09-07 00:51:51 -0700

    Update wsgi.py

--- personalWebsite/wsgi.py
+++ personalWebsite/wsgi.py
@@ -15,4 +15,4 @@ from whitenoise import WhiteNoise
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "personalWebsite.settings")
 
 application = get_wsgi_application()
-application = WhiteNoise(application, root=os.path.join(os.path.dirname(__file__), "staticfiles")
+application = WhiteNoise(application, root=os.path.join(os.path.dirname(__file__), "staticfiles"))
